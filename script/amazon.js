f946ec4746a46c0623753a218af4fd07fdea154d
Soumyajit-coder <soumyajit.dey8100@gmail.com>
2025-09-06 12:55:51 +0530

    Add product.js file

--- script/amazon.js
+++ script/amazon.js
@@ -1,33 +1,3 @@
-const products = [
-    {
-        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-        rating: {
-            star: "45",
-            count: "87"
-        },
-        price: "1090"
-    },
-    {
-        image: "images/products/intermediate-composite-basketball.jpg",
-        name: "Intermediate Size Basketball",
-        rating: {
-            star: "40",
-            count: "127"
-        },
-        price: "2095"
-    },
-    {
-        image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        name: "Adults Plain Cotton T-Shirt - 2 Pack",
-        rating: {
-            star: "45",
-            count: "56"
-        },
-        price: "799"
-    }
-];
-
 let productsListHtml = '';
 
 products.forEach((product) => {
@@ -46,12 +16,12 @@ products.forEach((product) => {
           <div class="product-rating-container">
             <img
               class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.star}.png"
+              src="images/ratings/rating-${product.rating.stars * 10}.png"
             />
             <div class="product-rating-count link-primary">${product.rating.count}</div>
           </div>
 
-          <div class="product-price">$${(product.price / 100).toFixed(2)}</div>
+          <div class="product-price">$${(product.priceCents / 100).toFixed(2)}</div>
 
           <div class="product-quantity-container">
             <select>
